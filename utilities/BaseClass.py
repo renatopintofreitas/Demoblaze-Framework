e80e1b1431f6a5f156e3d2eb70ffef39e88162aa
renatopintofreitas <renatopintofreitas@gmail.com>
2023-08-04 20:16:35 +0100

    fixed log file path

--- utilities/BaseClass.py
+++ utilities/BaseClass.py
@@ -10,7 +10,8 @@ class BaseClass:
         
         loggerName = inspect.stack()[1][3]
         logger = logging.getLogger(loggerName)
-        log_directory = r"D:\DevLibrary\python\Demoblaze Framework\reports"
+        #log_directory = r"D:\DevLibrary\python\Demoblaze Framework\reports"
+        log_directory = r"C:\REPORTS"
         log_filepath = os.path.join(log_directory, 'logfile.log')
         fileHandler = logging.FileHandler(log_filepath)
         formatter = logging.Formatter("%(asctime)s : %(levelname)s : %(name)s : %(message)s")
