b0e6aa26027bd93b12797c09ff69d2a07bfe7dff
renatopintofreitas <116327293+renatopintofreitas@users.noreply.github.com>
2023-08-09 12:09:05 +0100

    webdrivers path update

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,15 +16,15 @@ def setup(request):
     browser_name = request.config.getoption("browser_name")
     
     if browser_name == "chrome":
-        service_obj = Service("D:\DevLibrary\drivers\chromedriver.exe")
+        service_obj = Service("C:\drivers\chromedriver.exe")
         driver = webdriver.Chrome(service=service_obj)
         
     elif browser_name == "firefox":
-        service_obj = Service("D:\DevLibrary\drivers\geckodriver.exe")
+        service_obj = Service("C:\drivers\geckodriver.exe")
         driver = webdriver.Firefox(service=service_obj)
         
     elif browser_name == "edge":
-        service_obj = Service("D:\DevLibrary\drivers\msedgedriver.exe")
+        service_obj = Service("C:\drivers\msedgedriver.exe")
         driver = webdriver.Edge(service=service_obj)
     
     driver.implicitly_wait(5)
