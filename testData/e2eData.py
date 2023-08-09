dec8c9060f26942e731befc6dd58f82c8a503b04
renatopintofreitas <renatopintofreitas@gmail.com>
2023-08-09 17:26:55 +0100

    further fixes

--- testData/e2eData.py
+++ testData/e2eData.py
@@ -1,3 +1,3 @@
 class e2eData:
     
-    test_e2e_data = [{"email":"renato4@nether.com", "password":"abcabc", "product":"Iphone 6 32gb", "name":"renato", "country":"portugal", "city":"porto", "creditcard":"99999999999999999", "month": "6", "year":"1993"}]
+    test_e2e_data = [{"email":"renato4@nether.com", "password":"abcabc", "product":"Samsung galaxy s6", "name":"renato", "country":"portugal", "city":"porto", "creditcard":"99999999999999999", "month": "6", "year":"1993"}]
