d2b34b8810bcedb63a796ede15943689141e7e48
renatopintofreitas <renatopintofreitas@gmail.com>
2023-08-09 17:48:42 +0100

    further fixes

--- tests/test_e2e_loginbuy.py
+++ tests/test_e2e_loginbuy.py
@@ -49,6 +49,7 @@ class Test_e2e(BaseClass):
             if productName == getData["product"]:
                 log.info(productName)
                 product.find_element(By.XPATH, "div/h4/a").click()
+                time.sleep(1)
                 break
                 
                 
