3855b64e1d9c94114fdd53566a27f093baa8b4f4
renatopintofreitas <renatopintofreitas@gmail.com>
2023-08-09 16:23:48 +0100

    further fixes

--- tests/test_e2e_loginbuy.py
+++ tests/test_e2e_loginbuy.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import pytest
 import time        
         
         
